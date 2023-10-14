95bc34ae6607f3bf9d8d1a1f3d83e4a49f08fce2
Ugurb20 <ugurbukcuoglu@gmail.com>
2023-10-14 20:11:51 +0300

    animations

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,52 @@ import PerformanceMarketing from './sections/pmarketing';
 import GrowthConsulting from './sections/gconsulting';
 import AppAudit from './sections/appaudit';
 import Footer from './sections/footer';
+import { motion } from 'framer-motion';
 
 function App() {
+  const cardVariants = {
+    offscreen: {
+      x: -100,
+      opacity: 0,
+    },
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: 'spring',
+        bounce: 0.2,
+        duration: 0.8,
+      },
+    },
+  };
   return (
     <div style={{ width: '100%' }}>
       <Nav />
       <Hero />
-      <PerformanceMarketing />
-      <GrowthConsulting />
-      <AppAudit />
+      <motion.div
+        variants={cardVariants}
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ amount: 0.2 }}
+      >
+        <PerformanceMarketing />
+      </motion.div>
+      <motion.div
+        variants={cardVariants}
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ amount: 0.2 }}
+      >
+        <GrowthConsulting />
+      </motion.div>
+      <motion.div
+        variants={cardVariants}
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ amount: 0.2 }}
+      >
+        <AppAudit />
+      </motion.div>
       <Footer />
     </div>
   );
--- src/sections/pmarketing/index.jsx
+++ src/sections/pmarketing/index.jsx
@@ -3,8 +3,24 @@ import GoogleIcon from '../../assets/icons/googleIcon';
 import MetaIcon from '../../assets/icons/metaIcon';
 import TiktokIcon from '../../assets/svg/tiktok.svg';
 import style from './index.module.scss';
+import { motion } from 'framer-motion';
 
 export default function PerformanceMarketing() {
+  const cardVariants = {
+    offscreen: {
+      x: -100,
+      opacity: 0,
+    },
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: 'spring',
+        bounce: 0.2,
+        duration: 0.8,
+      },
+    },
+  };
   return (
     <div className={style.container} id="pmarketing">
       <div className={style.header}>
